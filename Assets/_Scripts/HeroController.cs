7378b924f180700a0f4038b890403cc491d20248
vips95 <arora.vips@gmail.com>
2015-10-26 18:50:15 -0400

    added player movement and jump

--- Assets/_Scripts/HeroController.cs
+++ Assets/_Scripts/HeroController.cs
@@ -29,8 +29,11 @@ public class HeroController : MonoBehaviour {
 	private Animator _animator; // we'll use this later
 	private AudioSource[] _audioSources;
 	private AudioSource _coinSound;
+	private AudioSource _jumpSound;
 	
-	private float _moving = 0;
+	private float _movingValue = 0;
+	private bool rightface = true;
+	private bool ground = true;
 	
 	// Use this for initialization
 	void Start () {
@@ -40,6 +43,7 @@ public class HeroController : MonoBehaviour {
 		
 		this._audioSources = gameObject.GetComponents<AudioSource> ();
 		this._coinSound = this._audioSources [0];
+		this._jumpSound = this._audioSources [1];
 	}
 	
 	void FixedUpdate () {
@@ -49,26 +53,46 @@ public class HeroController : MonoBehaviour {
 		float absVelX = Mathf.Abs (this._rigidBody2D.velocity.x);
 		float absVelY = Mathf.Abs (this._rigidBody2D.velocity.y);
 		
-		this._moving = Input.GetAxis ("Horizontal"); // value is between -1 and 1
+		this._movingValue = Input.GetAxis ("Horizontal"); // value is between -1 and 1
 		
-		if (this._moving != 0) {
-			this._animator.SetInteger("AnimState",1); //play walk clip
+		if (this._movingValue != 0) {
+
 			// we're moving
-			if(this._moving > 0) {
+			if(this._movingValue > 0) {
 				// moving right
 				if(absVelX < this.velocityRange.vMax) {
 					forceX = this.speed;
+					this.rightface = true;
+					this.turn ();
 				}
-			} else if (this._moving < 0) {
+			} else if (this._movingValue < 0) {
 				//moving left
 				if(absVelX < this.velocityRange.vMax) {
 					forceX = -this.speed;
+					this.rightface = false;
+					this.turn ();
 				}
 			}
-		} else if (this._moving == 0) {
+
+		} else if (this._movingValue == 0) {
 			// we're idle
-			this._animator.SetInteger("AnimState",0);
+			this._animator.SetInteger("AnimState",0); // player walk and idle 
 		}
+		//Check if player is jumping
+		if ((Input.GetKey ("up") || Input.GetKey (KeyCode.Space))) { //if player grounded
+			if (this.ground) {     //player is jumping
+				 
+				if (absVelY < this.velocityRange.vMax) {
+					this._animator.SetInteger("AnimState",1);
+					forceY = this.jump;
+					this._jumpSound.Play();
+					this.ground = false;
+				}
+			}
+
+
+		}
+
 		
 		this._rigidBody2D.AddForce (new Vector2 (forceX, forceY));
 	}
@@ -76,6 +100,25 @@ public class HeroController : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D otherCollider) {
 		if (otherCollider.gameObject.CompareTag ("Coin")) {
 			this._coinSound.Play();
+		}
+	}
+	void OnCollisionStay2D(Collision2D otherCollider) {
+				if (otherCollider.gameObject.CompareTag ("Platform")) {
+						this.ground = true;
+				}
+	}
+	//Private methods
+
+	private void turn()
+	{
+		/*rightface = !rightface;
+		Vector3 theScale = _transform.localScale;
+		theScale.x *= -1;
+		_transform.localScale = theScale; */
+		if (this.rightface) {
+			this._transform.localScale = new Vector3(.5f,.5f,.5f); // turn to normal
+		} else {
+			this._transform.localScale = new Vector3(-.5f,.5f,.5f);
 		}
 	}
 }
